65af9899981ab5b00c7eceac12ae61d8ef1d5182
Morten Brudvik <morten@brudvik.com>
2020-05-28 20:32:46 +0200

    Added support for dragging and dropping files into the Files textbox

--- WixFileGeneratorWpf/MainWindow.xaml.cs
+++ WixFileGeneratorWpf/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using MahApps.Metro.Controls.Dialogs;
 
@@ -11,11 +14,14 @@ namespace WixFileGeneratorWpf
         public MainWindow()
         {
             InitializeComponent();
+
+            FileTextInput.Drop += FileTextInput_Drop;
+            FileTextInput.PreviewDragOver += FileTextInput_PreviewDragOver;
         }
 
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
-            var inputText = TextInput.Text;
+            var inputText = FileTextInput.Text;
             var sourcePrefix = SourcePrefix.Text;
 
             try
@@ -39,6 +45,28 @@ namespace WixFileGeneratorWpf
             {
                 this.ShowMessageAsync("An error occured when processing text.", exception.Message);
             }
+        }
+
+
+        private void FileTextInput_PreviewDragOver(object sender, DragEventArgs e) // TRICK: To allow other data objects then strings
+        {
+            e.Handled = true; 
+        }
+
+        private void FileTextInput_Drop(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop)) return;
+            
+            var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            foreach (var file in files)
+            {
+                if(Directory.Exists(file))
+                    continue;
+
+                FileTextInput.Text += Environment.NewLine + Path.GetFileName(file);
+            }
+            FileTextInput.Text = Regex.Replace(FileTextInput.Text, @"^\s+$[\r\n]*", string.Empty, RegexOptions.Multiline);
         }
     }
 }
